fa8a2309666ae560fc887d45717a6046dc793bdc
netsinfoo <netsinfoo@gmail.com>
2020-06-12 17:54:55 -0300

    corrigido

--- ruby/Aprendendo/edabit/cannot_capture.rb
+++ ruby/Aprendendo/edabit/cannot_capture.rb
@@ -2,20 +2,20 @@ def cannot_capture(board)
 	for i in 0..7
 		for j in 0..7
 			if board[i][j] == 1
-				if      i + 2 < 8 &&  j + 1 < 8  &&  board[i+2][j+1] == 1 
+				if      i + 2 < 8 &&  j + 1 < 8  &&  board[i+2][j+1] == 1
 					return false
 
 				elsif	i + 2 < 8 &&  j - 1 >=0 &&  board[i+2][j-1] == 1
 						return false
-												
+
 			    elsif j + 2 < 8 && i - 1 >=0  && board[i-1][j+2] == 1
 						return false
 
-				elsif j + 2 < 8 && i + 1 < 8 && board[i+1][j+2] == 1 
+				elsif j + 2 < 8 && i + 1 < 8 && board[i+1][j+2] == 1
 						return false
-				
+
 				else
-					
+
 				end
 			end
 		end
@@ -32,5 +32,3 @@ cannot_capture([
   [0, 1, 1, 0, 0, 0, 0, 1],
   [0, 0, 0, 0, 0, 0, 0, 0]
 ])
-
-
