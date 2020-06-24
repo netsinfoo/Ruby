062fb795d87ba9db84754771f1d85321ca806dcd
Netsinfoo <netsinfoo@gmail.com>
2020-06-24 09:55:49 -0300

    email ducplicado

--- ruby/Aprendendo/rails/sample_app/db/migrate/20200624115828_add_index_to_users_email.rb
+++ ruby/Aprendendo/rails/sample_app/db/migrate/20200624115828_add_index_to_users_email.rb
@@ -1,4 +1,7 @@
+# Adiciona ao banco um nivel de tratamento que, caso a validação de uniqueness do email
+# passe o banco impede do registro duplo.
 class AddIndexToUsersEmail < ActiveRecord::Migration
   def change
+    add_index :users, :email, unique: true
   end
 end
--- ruby/Aprendendo/rails/sample_app/test/models/user_test.rb
+++ ruby/Aprendendo/rails/sample_app/test/models/user_test.rb
@@ -7,31 +7,31 @@ class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: "Example User", email: "user@example.com")
   end
-  
+  # testa se o nome é valido
   test "should be valid" do
     assert @user.valid?
   end
-  
+  # testa se o nome esta presente
   test "name should be present" do
     @user.name = "    "
     assert_not @user.valid?
   end
-
+# testa se o email está presente
   test "email should be present" do
     @user.email = "    "
     assert_not @user.valid?
   end
-
+# testa se o nome não exede o limite max
   test "name should not be too long" do
     @user.name = "a" * 51 
     assert_not @user.valid?
   end
-
+# testa se o email exede o limite max
   test "email should not be too long" do
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
-
+# Testa a validade o nome do email
   test "email validation should accept valid addresses" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
       first.last@foo.jp alice+bob@baz.cn]
@@ -40,7 +40,7 @@ class UserTest < ActiveSupport::TestCase
         assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
-  
+  # Testa o padrão do elemento email se vem com algum caractere improprio
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org 
       user.name@example.foo@bar_baz.com foo@bar+baz.com]
@@ -48,5 +48,12 @@ class UserTest < ActiveSupport::TestCase
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
+  end
+# Testa se o email utilizado é unico. No caso se ele já esta sendo usando.
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save 
+    assert_not duplicate_user.valid?
   end
 end
